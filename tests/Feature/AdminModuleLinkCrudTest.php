bef59d0ddeecca415132638f566e37d0a10bde3b
Marcus Covington <marcuscovington@gmail.com>
2026-05-15 01:27:12 +0000

    Fix duplicate setup in module link CRUD test

--- tests/Feature/AdminModuleLinkCrudTest.php
+++ tests/Feature/AdminModuleLinkCrudTest.php
@@ -7,11 +7,20 @@ use App\Models\Organization;
 use App\Models\OrganizationModuleLink;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Tests\TestCase;
 
 class AdminModuleLinkCrudTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware(ValidateCsrfToken::class);
+    }
 
     private function adminUser(): User
     {
